7842eb18b27de76ea423404d7943d25d4ebb7e8d
suanzhai <343628123@qq.com>
2025-06-13 13:17:40 +0800

    文件路径修改

--- src/main.py
+++ src/main.py
@@ -21,13 +21,13 @@ def main():
 - 产品B: 市场定位
 ![未来增长](images/forecast.png)
         """
-    template_path = "templates/Fair frames presentation.pptx"
+    template_path = "../templates/Fair frames presentation.pptx"
     presentation = loader.TemplateLoader.load_template(template_path)
     lay_mapping = loader.TemplateLoader.get_layout_mapping(presentation)
     loader.TemplateLoader.print_layouts(presentation)
 
     power_point = power_point_parser.PowerPointParser.parse(input_text, lay_mapping)
-    output_pptx_file_path = f"outputs/{power_point.title}.pptx"
+    output_pptx_file_path = f"../outputs/{power_point.title}.pptx"
     ppt.genarater.PPTGenerator.generate(power_point, template_path, output_pptx_file_path)
 
 
